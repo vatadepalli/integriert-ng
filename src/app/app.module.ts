14aa8167f64bc41f31e05d63bdfe8c69c92c74e3
Vijaya Aditya <vatadepalli@hotmail.com>
2020-02-27 02:32:29 +0530

    stuff

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,7 +18,7 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MainComponent } from './components/layout/main/main.component';
-import { MatGridListModule } from '@angular/material';
+import { MatGridListModule, MatFormFieldModule, MatInputModule } from '@angular/material';
 import { WhatWeDoComponent } from './components/pages/what-we-do/what-we-do.component';
 import { SolutionsComponent } from './components/pages/solutions/solutions.component';
 import { PartnersComponent } from './components/pages/partners/partners.component';
@@ -55,7 +55,9 @@ import { BannerComponent } from './components/layout/main/banner/banner.componen
     MatSidenavModule,
     MatIconModule,
     MatListModule,
-    MatGridListModule
+    MatGridListModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/pages/contact/contact.component.ts
+++ src/app/components/pages/contact/contact.component.ts
@@ -1,13 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
+
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent implements OnInit {
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches),
+      shareReplay()
+    );
 
-  constructor() { }
+
+  constructor(private breakpointObserver: BreakpointObserver) { }
 
   ngOnInit() {
   }
